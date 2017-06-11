6e87293738a8d47ce6a7b89dd7bc13258c8e98ee
Tobias Schifftner <ts@ambimax.de>
2017-06-11 15:35:59 +0200

    Fixed phpcs warning

--- src/app/code/community/Ambimax/DisableMediaRemoval/Model/Resource/Product/Attribute/Backend/Media.php
+++ src/app/code/community/Ambimax/DisableMediaRemoval/Model/Resource/Product/Attribute/Backend/Media.php
@@ -7,11 +7,11 @@ class Ambimax_DisableMediaRemoval_Model_Resource_Product_Attribute_Backend_Media
      * Remove duplicates
      *
      * @param array $result
-     * @return Mage_Catalog_Model_Resource_Product_Attribute_Backend_Media
+     * @return Mage_Catalog_Model_Resource_Product_Attribute_Backend_Media|$this
      */
     protected function _removeDuplicates(&$result)
     {
-        if(Mage::getStoreConfigFlag('catalog/frontend/disable_media_removal')) {
+        if (Mage::getStoreConfigFlag('catalog/frontend/disable_media_removal')) {
             return $this;
         }
 
